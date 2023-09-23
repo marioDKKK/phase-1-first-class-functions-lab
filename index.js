177e2a55f29730a129b2732e17e8d42392d146a4
mario <kinuthiamariodavis@gmail.com>
2023-09-23 15:17:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+// Code your solution in this file!
+const returnFirstTwoDrivers =  function(arr){ return arr.slice(0,2)};
+const returnLastTwoDrivers =  function(arr){ return arr.slice(-2)};
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(val){
+       return val => val*val;
+}
+
+function fareDoubler(fare){
+   const doubler = createFareMultiplier();
+
+   return fare*2;
+
+}
+function fareTripler(fare){
+   const tripl = createFareMultiplier();
+   return fare*3;
+}
+
+
+
+function selectDifferentDrivers(driver, func){
+
+   if(func === returnFirstTwoDrivers){
+       return returnFirstTwoDrivers(driver);
+   }else if (func === returnLastTwoDrivers){
+       return returnLastTwoDrivers(driver);
+   }
+
+}
+
+
+// Hoisting
+willItHoist();
+
+// Function expression
+const willItHoist = function () {
+ console.log("You tell me!");
+};
+
+
+
